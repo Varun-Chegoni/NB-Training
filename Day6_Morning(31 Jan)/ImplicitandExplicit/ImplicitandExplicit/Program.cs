d74893574ba8d18225fa84329c98ae7abd0c17ec
Varun-Chegoni <varunsaikumar18@gmail.com>
2022-01-31 15:22:33 +0530

    Day6 Submit

---
+++ Day6_Morning(31 Jan)/ImplicitandExplicit/ImplicitandExplicit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitandExplicit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Program for Implicit and Explicit by Varun ");
+            Console.WriteLine();
+
+            // Implicit 
+            short a = 20;
+            int b = a;
+            Console.WriteLine(b);
+
+            // Explicit
+            int p = 20;
+            short q = (short) p;
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+        }
+    }
+}
